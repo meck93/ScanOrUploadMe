99efd514595b46b9bc067b40ba6d2c774f0261e5
Moritz Eck <moritz.eck@gmail.com>
2019-02-20 14:26:03 +0100

    #47 #52: transform Vision and NLP request to return a promise

--- server/src/services/gcvocr.js
+++ server/src/services/gcvocr.js
@@ -1,25 +1,54 @@
-// import google cloud vision libraries
-const gcv = require("@google-cloud/vision");
+// Imports the Google Cloud client library
+const vision = require("@google-cloud/vision").v1p1beta1;
 
 // google cloud vision ocr
-async function getTextFromVision(pathToImage) {
+async function getTextFromImage(pathToImage, lang) {
   // create a client and authenticate
-  const client = new gcv.ImageAnnotatorClient({
+  const client = new vision.ImageAnnotatorClient({
     keyFilename: process.env.PATH_TO_CREDENTIALS
   });
 
-  // Performs label detection on the image file
-  client
-    .textDetection(pathToImage)
-    .then(response => {
-      // print text detection result
-      console.log(response[0].textAnnotations[0]);
-      const text = response[0].textAnnotations[0].description;
-      console.log(text);
-    })
-    .catch(err => {
-      console.error("ERROR:", err);
-    });
+  const request = {
+    image: {
+      source: { imageUri: pathToImage }
+    },
+    imageContext: {
+      languageHints: [lang]
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    // detect the text in the image
+    client
+      .textDetection(request)
+      .then(result => {
+        const response = result[0];
+        // if the request was successfull but contains an error object
+        // resolve the error object and handle it further down
+        if (response.error) {
+          console.log(
+            "GC-VISION-ERROR: request successfull but contains error object\n",
+            response.error
+          );
+          resolve(response);
+          // request was successfull and contains no error object
+        } else {
+          // extract the relevant information to be returned
+          const res = {
+            locations: response.textAnnotations[0].locations,
+            description: response.textAnnotations[0].description,
+            locale: response.textAnnotations[0].locale,
+            confidence: response.textAnnotations[0].confidence
+          };
+          resolve(res);
+        }
+      })
+      .catch(err => {
+        // request was unsuccessfull
+        console.error("ERROR:", err);
+        reject(err);
+      });
+  });
 }
 
-export { getTextFromVision };
+export { getTextFromImage };
