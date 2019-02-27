f19e99be49b0e2322b0b8c7a44ef821023e47c53
Moritz Eck <moritz.eck@gmail.com>
2019-02-27 15:20:47 +0100

    add: second upload method to google cloud vision #55

--- server/src/services/gcvocr.js
+++ server/src/services/gcvocr.js
@@ -47,4 +47,50 @@ async function getTextFromImage(pathToImage, lang) {
   });
 }
 
-export { getTextFromImage };
+// google cloud vision ocr
+async function getTextFromImageBase64(data, lang) {
+  // create a client and authenticate
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: process.env.PATH_TO_CREDENTIALS
+  });
+
+  const request = {
+    image: {
+      content: data
+    },
+    imageContext: {
+      languageHints: [lang]
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    // detect the text in the image
+    client
+      .textDetection(request)
+      .then(result => {
+        const response = result[0];
+        // if the request was successfull but contains an error object
+        // resolve the error object and handle it further down
+        if (response.error) {
+          resolve(response);
+          // request was successfull and contains no error object
+        } else {
+          // extract the relevant information to be returned
+          const res = {
+            locations: response.textAnnotations[0].locations,
+            description: response.textAnnotations[0].description,
+            locale: response.textAnnotations[0].locale,
+            confidence: response.textAnnotations[0].confidence
+          };
+          resolve(res);
+        }
+      })
+      .catch(err => {
+        // request was unsuccessfull
+        console.error("ERROR:", err);
+        reject(err);
+      });
+  });
+}
+
+export { getTextFromImage, getTextFromImageBase64 };
