b978db7ef15e4b18e4ff75e10f2622183b2b31a8
Moritz Eck <moritz.eck@gmail.com>
2019-03-01 18:39:10 +0100

    cleaned up: CalendarEventScreen

--- app/src/screens/CalendarEventScreen.js
+++ app/src/screens/CalendarEventScreen.js
@@ -9,7 +9,7 @@ import {
   Alert
 } from "react-native";
 import { withNavigation } from "react-navigation";
-import { Calendar } from "expo";
+import { Calendar, Notifications } from "expo";
 
 // redux
 import { connect } from "react-redux";
@@ -47,31 +47,9 @@ class CalendarEventScreen extends React.Component {
         </Text>
 
         <Text style={styles.textContainer}>
-          All Events:
-          {this.props.calendar.events.map(event => (
-            <Text key={event.id} style={styles.textContainer}>
-              Event: {JSON.stringify(event.id)} - Description:{" "}
-              {JSON.stringify(event.description)}
-            </Text>
-          ))}
-        </Text>
-
-        <Text style={styles.textContainer}>
           Current Event:
-          {JSON.stringify(this.props.calendar.currentEvent.description)}
+          {JSON.stringify(this.props.calendar.currentEvent)}
         </Text>
-
-        <View style={styles.container}>
-          <View style={styles.buttonContainer}>
-            {this.props.calendar.events.map(event => (
-              <Button
-                key={event.id}
-                title={`Set current event to: ${event.id}`}
-                onPress={() => this.props.setCurrentEvent(event.id)}
-              />
-            ))}
-          </View>
-        </View>
 
         <TextInput
           style={{ height: 40 }}
@@ -79,30 +57,35 @@ class CalendarEventScreen extends React.Component {
           value={this.state.summary}
           onChangeText={text => this.setState({ summary: text })}
         />
+
         <TextInput
           style={{ height: 40 }}
           placeholder={"DESCRIPTION"}
           value={this.state.description}
           onChangeText={text => this.setState({ description: text })}
         />
+
         <TextInput
           style={{ height: 40 }}
           placeholder={"LOCATION:"}
           value={this.state.location}
           onChangeText={text => this.setState({ location: text })}
         />
+
         <TextInput
           style={{ height: 40 }}
           placeholder={"START_TIME"}
           value={this.state.startTime}
           onChangeText={text => this.setState({ startTime: text })}
         />
+
         <TextInput
           style={{ height: 40 }}
           placeholder={"END_TIME"}
           value={this.state.endTime}
           onChangeText={text => this.setState({ endTime: text })}
         />
+
         <View style={styles.container}>
           <View style={styles.buttonContainer}>
             <Button onPress={this._addToCalendar} title="Add to Calendar" />
@@ -111,9 +94,6 @@ class CalendarEventScreen extends React.Component {
           <View style={styles.buttonContainer}>
             <Button onPress={this._deleteEvent} title="Remove Event" />
           </View>
-        <View style={styles.buttonContainer}>
-        <Button onPress={this._saveEvent} title="Save" />
-        </View>
         </View>
       </ScrollView>
     );
@@ -174,85 +154,6 @@ class CalendarEventScreen extends React.Component {
     }
   };
 
-  _saveEvent = async () => {
-    const userId = '8ba790f3-5acd-4a08-bc6a-97a36c124f29';
-    /*const saveUserId = async userId => {
-      try {
-        await AsyncStorage.setItem('userId', userId);
-        console.log('*****************Success!*****************');
-      } catch (error) {
-        // Error retrieving data
-        console.log(error.message);
-      }
-    };*/
-    /*try {
-      await AsyncStorage.setItem('userId', JSON.stringify(userId));
-      console.log('*****************Success!*****************');
-    } catch (error) {
-      // Error saving data
-      console.log({ error });
-      console.error(error);
-    }*/
-    //create dummy event
-    let eventDetails = {
-      title: "I am your new event", //
-      startDate: "2019-02-19T15:00:00.000Z", ///this.state.start, //got error saying saying it expected date to end in Z so edited: "2019-02-19T15:00:00.000Z+01:00",
-      endDate: "2019-02-19T16:00:00.000Z",
-      allDay: false,
-      location: this.state.location,
-      notes: "Testing add to Calendar",
-      alarms: [
-        {
-          relativeOffset: -15,
-          method: Calendar.AlarmMethod.DEFAULT
-        }
-      ],
-      recurrenceRule: {
-        frequency: Calendar.Frequency.DAILY,
-        interval: 2,
-        endDate: "2019-02-22T16:00:00.000Z",
-        occurrence: 4
-      },
-      availability: "busy",
-      // TODO: check if the timezone still works on Android
-      timeZone: "GMT+1",
-      url: "http://www..."
-    };
-
-    try {
-      if (this.state.startTime) {
-        eventDetails.title = this.state.description;
-        eventDetails.location = this.state.location;
-        eventDetails.startDate = new Date(this.state.startTime);
-        eventDetails.endDate = new Date(this.state.endTime);
-        eventDetails.recurrenceRule.endDate = new Date(this.state.endTime);
-        eventDetails.notes = this.state.summary;
-      }
-      //add event to default calendar
-      /*const eventId = await Calendar.createEventAsync(
-          Calendar.DEFAULT,
-          eventDetails
-      );*/
-      try {
-        //TODO: generate a proper key not, just 'userId' all the time
-        await AsyncStorage.setItem('userId', JSON.stringify(eventDetails));
-        console.log('*****************Success!*****************');
-      } catch (error) {
-        // Error saving data
-        console.log({ error });
-        console.error(error);
-      }
-      //if (this.state.eventId) {
-       // const id = this.state.eventId;}
-      //this.setState({ eventId: eventId });
-      //console.log("Event Id", eventId);
-
-      Alert.alert(`The ${eventDetails.title} was saved to this app!`);
-    } catch (error) {
-      console.log("Error", error);
-    }
-  };
-
   _addToCalendar = async () => {
     //create dummy event
     let eventDetails = {
@@ -268,7 +169,7 @@ class CalendarEventScreen extends React.Component {
           method: Calendar.AlarmMethod.DEFAULT
         }
       ],
-        //this piece of code adds event to every second day after the start date(because of the inteval 2)
+      //this piece of code adds event to every second day after the start date (because of the inteval 2)
       recurrenceRule: {
         frequency: Calendar.Frequency.DAILY,
         interval: 2,
@@ -310,17 +211,17 @@ class CalendarEventScreen extends React.Component {
     }
 
     let notificationId = Notifications.scheduleLocalNotificationAsync(
-    {
-      title: eventDetails.title,
-      body: 'Do not forget about your event on ' +eventDetails.startDate,
-    },
-    {
-      //repeat: 'minute',
-      time: new Date().getTime() + 10000,
-    },
-  );
-  console.log(notificationId);
-  //Notifications.cancelAllScheduledNotificationsAsync()
+      {
+        title: eventDetails.title,
+        body: "Do not forget about your event on " + eventDetails.startDate
+      },
+      {
+        //repeat: 'minute',
+        time: new Date().getTime() + 10000
+      }
+    );
+    console.log(notificationId);
+    //Notifications.cancelAllScheduledNotificationsAsync()
   };
 }
 
@@ -354,7 +255,7 @@ const styles = StyleSheet.create({
     marginBottom: 20
   },
   buttonContainer: {
-    marginTop:50,
+    marginTop: 50,
     marginHorizontal: 5
   }
 });
