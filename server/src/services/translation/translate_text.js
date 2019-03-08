229a3590eba45cb5b13bdf532d7d57009e959d2a
Tyson McLeod <tyson.mcleod.8851@student.uu.se>
2019-03-08 12:50:26 +0100

    Create translate_text.js
    Small function for translating text that will be used in the translation process.
    
    Co-Authored-By: Johan Isaksson <jisaksson94@gmail.com>

---
+++ server/src/services/translation/translate_text.js
@@ -0,0 +1,39 @@
+const {Translate} = require('@google-cloud/translate');
+const delay = require('delay');
+// receives target language and translates text to english
+async function translateFunc(ocrOutput){
+  // instantiate a client
+  const translate = new Translate({
+    keyFilename: process.env.PATH_TO_CREDENTIALS
+  });
+  // translate target lang = english
+  const target = 'en';
+  // text to translate
+  const text = ocrOutput;
+
+  return new Promise((resolve, reject) => {
+    // translate
+    translate
+      .translate(text, target)
+      .then(results => {
+        const translatedText = results[0];
+        resolve(translatedText)
+
+        if (translatedText.error){
+          resolve(translatedText);
+        } else {
+          const translatedText_1 = results[0];
+          resolve(translatedText_1);
+          }
+        })
+        .catch(err=>{
+          console.log("ERROR", err);
+          reject(err);
+        });
+  });
+}
+
+const test = "Hey du är bäst";
+translateFunc(test).then(function(result){
+  console.log(result);
+});
