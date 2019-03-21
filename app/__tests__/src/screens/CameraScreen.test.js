018d4ed9941dcebb7a1933f9d88c7b5fdfd864f6
Unknown <andra_stefania@yahoo.com>
2019-03-21 12:49:43 +0100

    testing CameraScreen and SavedEventsScreen

---
+++ app/__tests__/src/screens/CameraScreen.test.js
@@ -0,0 +1,88 @@
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import sinon from 'sinon';
+import { CameraScreen } from "../../../src/screens/CameraScreen";
+
+import React from "react";
+
+import renderer from "react-test-renderer";
+import Enzyme from "enzyme/build";
+import Adapter from "enzyme-adapter-react-16/build";
+
+
+describe("CameraScreen: Tests", () => {
+    const noPhoto = {
+            calendarEvent: null,
+            image: null,
+            uploading: false,
+        /*currentEventId: null,
+        currentEvent: null,
+        events: [] */
+    };
+
+    const dummyPhoto = {
+        calendarEvent: 11,
+        image: 'smth',
+        uploading: true,
+    };
+
+    const dummyState = {
+        id: 12312312,
+        description: "DummyTitle",
+        summary: "DummyText",
+        location: "DummyLocation",
+        startTime: "2019-02-27T16:00:00.000Z",
+        endTime: "2019-02-27T18:00:00.000Z"
+    };
+
+    const initializedState = {
+        id: 1,
+        description: "Test",
+        summary: "Test",
+        location: "Test",
+        startTime: "Test",
+        endTime: "Test"
+    };
+// Setup Enzyme Adapter
+   // Enzyme.configure({ adapter: new Adapter() });
+    jest.useFakeTimers();
+
+
+    beforeEach(() => {});
+
+
+    describe("CameraScreen: Shallow Render Component", () => {
+        it("CameraScreen: should render the component without an input photo ", () => {
+            const wrapper = shallow(<CameraScreen events ={noPhoto} />);
+            const component = wrapper.instance();
+
+            expect(component.state).toEqual(noPhoto);
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+
+        it("CameraScreen: should render the component with a mock input photo", () => {
+            const wrapper = shallow(<CameraScreen events={noPhoto} />);
+            const component = wrapper.instance();
+
+            const newState = {
+                calendarEvent: null,
+                image: null,
+                uploading: false,
+            };
+
+            component.setState({
+                calendarEvent: 11,
+                image: 'smth',
+                uploading: true
+            });
+
+            wrapper.update();
+
+            expect(component.state).toEqual(dummyPhoto);
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+    });
+
+
+
+});
---
+++ app/__tests__/src/screens/SavedEventsScreen.test.js
@@ -0,0 +1,148 @@
+import { shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import { SavedEventsScreen } from "../../../src/screens/SavedEventsScreen";
+
+import React from "react";
+import renderer from "react-test-renderer";
+import Enzyme from "enzyme/build";
+import Adapter from "enzyme-adapter-react-16/build";
+
+let now = new Date();
+let start = new Date(now.setHours(now.getHours() + 1)).toString();
+let end = new Date(now.setHours(now.getHours() + 2)).toString();
+
+let calendarEvent = {
+    description: "Guava Peeling Party",
+    id: 1234,
+    picUri: "https://t2.rbxcdn.com/34e222a0bf98d0a46de65de2ece139eb",
+    summary:
+        "Hi John\nI would like to invite you to my birthday party!\nBest James",
+    location: "Klostergatan, Uppsala, Sweden",
+    startTime: start,
+    endTime: end,
+    reminders: {
+        overrides: [{ method: "popup", minutes: 15 }],
+        useDefault: false
+    },
+    updated: Date()
+};
+
+let calendarEvent2 = {
+    description: "Puppy Petting Party",
+    id: 5555,
+    picUri: "https://t2.rbxcdn.com/34e222a0bf98d0a46de65de2ece139eb",
+    summary:
+        "Hi John\nI would like to invite you to my Puppy Petting Party!\nBest James",
+    location: "Klostergatan, Uppsala, Sweden",
+    startTime: start,
+    endTime: end,
+    reminders: {
+        overrides: [{ method: "popup", minutes: 15 }],
+        useDefault: false
+    },
+    updated: Date()
+};
+
+let calendarEvent3 = {
+    description: "Confetti Party",
+    id: 5050,
+    picUri: "https://t2.rbxcdn.com/34e222a0bf98d0a46de65de2ece139eb",
+    summary:
+        "Hi ho, hi,ho \nI would like to invite you to my Puppy Petting Party!\nBest James",
+    location: "Klostergatan, Uppsala, Sweden",
+    startTime: start,
+    endTime: end,
+    reminders: {
+        overrides: [{ method: "popup", minutes: 15 }],
+        useDefault: false
+    },
+    updated: Date()
+};
+
+/*
+* Before running the tests, go to SavedEventsScreen.js in render():
+*  comment out the lines I commented out below, and replace them with : this.props.events.events (like below):
+*           data={
+                // this.props.events.events.length === 0
+                //  ? this.state.data
+              //: this.props.events.events
+            this.props.events.events
+          }
+*/
+describe("SavedEventsScreen: Tests", () => {
+    const noEvents = {
+        data: [],
+        imageUri: null,
+        imageId: null,
+        displayImage: false
+    };
+
+    const someEvents={
+        data: [calendarEvent,calendarEvent2,calendarEvent3],
+        imageUri: 'herehere',
+        imageId: 1234,
+        displayImage: true
+    };
+
+
+// Setup Enzyme Adapter
+    Enzyme.configure({ adapter: new Adapter() });
+    jest.useFakeTimers();
+
+
+    beforeEach(() => {
+        wrapper = shallow(
+            <SavedEventsScreen events={noEvents}  />,
+        { disableLifecycleMethods: true }
+    );
+    });
+
+    describe("SavedEventsScreen: Shallow Render Component", () => {
+        it("Saved events: renders the component without any real events", () => {
+            const wrapper = shallow(<SavedEventsScreen events ={noEvents}  />);
+            //{ disableLifecycleMethods: true }
+            const component = wrapper.instance();
+
+            const newState = {
+                data: null,
+                imageUri: null,
+                imageId: null,
+                displayImage: false
+
+            };
+            component.setState({
+                data: null,
+            });
+            wrapper.update();
+
+            expect(component.state).toEqual(newState);
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+
+        it("Saved events: renders the component with a list of events", () => {
+            const wrapper = shallow(<SavedEventsScreen events={noEvents}/>);
+            const component = wrapper.instance();
+            const newState = {
+                data: null,
+                imageUri: null,
+                imageId: null,
+                displayImage: false
+
+            };
+            component.setState({
+                data: [calendarEvent,calendarEvent2,calendarEvent3],
+                imageUri: 'herehere',
+                imageId: 1234,
+                displayImage: true
+            });
+            wrapper.update();
+
+            expect(component.state).toEqual(someEvents);
+            expect(toJson(wrapper)).toMatchSnapshot();
+        });
+    });
+
+
+
+});
