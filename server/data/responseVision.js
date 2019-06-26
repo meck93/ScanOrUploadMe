f4d0e0bde90f0cf71b0a8af1bfdf7a57d9c6f16a
Moritz Eck <moritz.eck@gmail.com>
2019-06-26 12:23:12 +0200

    clean up server-side tests

--- server/data/responseVision.js
+++ server/data/responseVision.js
@@ -1,19 +1,14 @@
 const response = {
   locations: [],
   properties: [],
-  mid: "",
-  locale: "en",
+  mid: '',
+  locale: 'en',
   description:
-    "Travelling with luggage\nYou may only bring on board as much\nluggage as you can easily carry yourself.\n",
+    'Travelling with luggage\nYou may only bring on board as much\nluggage as you can easily carry yourself.\n',
   score: 0,
   confidence: 0,
   topicality: 0,
   boundingPoly: {
-    vertices: [
-      { x: 22, y: 36 },
-      { x: 501, y: 36 },
-      { x: 501, y: 167 },
-      { x: 22, y: 167 }
-    ]
+    vertices: [{ x: 22, y: 36 }, { x: 501, y: 36 }, { x: 501, y: 167 }, { x: 22, y: 167 }]
   }
 };
--- server/test/calendarServiceTest.js
+++ server/test/calendarServiceTest.js
@@ -1,69 +1,72 @@
-import { createCalendarEvent } from "../src/services/calendar/calendarService.js";
+import { createCalendarEvent } from '../src/services/calendar/calendarService.js';
 import { assert, expect } from 'chai';
 
 const nlpResponse = require('../src/fakeData/mockNlpResponse.js');
 
 // Create fake OCR result
 const ocrRes = {
-    locations: "en",
-    description: "Dearest Peter  O'Toole, your dentist appointment is on Saturday afternoon 15:30-16-30",
-    locale: "en",
-    confidence: 0.5
+  locations: 'en',
+  description: "Dearest Peter  O'Toole, your dentist appointment is on Saturday afternoon 15:30-16-30",
+  locale: 'en',
+  confidence: 0.5
 };
 
 // Prepare fake NLP response from the fakeData folder
 let nlpObject = nlpResponse.default[0].entities;
 
 describe('calendarService', function() {
-
-    describe('Undefined arguments', function() {
-        it('should throw TypeError for undefined nlpResponse', function() {
-            expect(() => {
-                createCalendarEvent(undefined);
-            }).to.throw(TypeError, "Undefined input");
-        });
+  describe('Undefined arguments', function() {
+    it('should throw TypeError for undefined nlpResponse', function() {
+      expect(() => {
+        createCalendarEvent(undefined);
+      }).to.throw(TypeError, 'Undefined input');
     });
+  });
 
-    describe('Highest confidence', function(){
-        it('the entity with the highest salience should be selected', function(){
-            let multiplePerson = [nlpObject[4], nlpObject[1], nlpObject[2]];
-            assert.notEqual(createCalendarEvent(multiplePerson, ocrRes).description, "Dentist appointment, Less credible T.E. Lawrence");
-        });
+  describe('Highest confidence', function() {
+    it('the entity with the highest salience should be selected', function() {
+      let multiplePerson = [nlpObject[4], nlpObject[1], nlpObject[2]];
+      assert.notEqual(
+        createCalendarEvent(multiplePerson, ocrRes).description,
+        'Dentist appointment, Less credible T.E. Lawrence'
+      );
     });
+  });
 
-    describe('Complete summary', function(){
-        it('optimal description should consist of: event, organization, person', function(){
-
-            assert.equal(createCalendarEvent(nlpObject, ocrRes).description, "Dentist appointment, Peter O Toole, T.E. Lawrence");
-        });
+  describe('Complete summary', function() {
+    it('optimal description should consist of: event, organization, person', function() {
+      assert.equal(
+        createCalendarEvent(nlpObject, ocrRes).description,
+        'Dentist appointment, Peter O Toole, T.E. Lawrence'
+      );
     });
+  });
 
-    describe('No location', function(){
-        it('no location in the NLP object should result in default location: UNSPECIFIED', function(){
-            let noLocation = [nlpObject[0], nlpObject[1], nlpObject[2], nlpObject[4], nlpObject[5]];
-            assert.equal(createCalendarEvent(noLocation, ocrRes).location, "UNSPECIFIED");
-        });
+  describe('No location', function() {
+    it('no location in the NLP object should result in default location: UNSPECIFIED', function() {
+      let noLocation = [nlpObject[0], nlpObject[1], nlpObject[2], nlpObject[4], nlpObject[5]];
+      assert.equal(createCalendarEvent(noLocation, ocrRes).location, 'UNSPECIFIED');
     });
+  });
 
-    describe('No event', function(){
-        it('no event in the NLP object should result is default description: EVENT CREATION RESULT', function(){
-            let noEvent = [nlpObject[0], nlpObject[1], nlpObject[2], nlpObject[3], nlpObject[5]];
-            assert.equal(createCalendarEvent(noEvent, ocrRes).description, "EVENT CREATION RESULT");
-        });
+  describe('No event', function() {
+    it('no event in the NLP object should result is default description: EVENT CREATION RESULT', function() {
+      let noEvent = [nlpObject[0], nlpObject[1], nlpObject[2], nlpObject[3], nlpObject[5]];
+      assert.equal(createCalendarEvent(noEvent, ocrRes).description, 'EVENT CREATION RESULT');
     });
+  });
 
-    describe('No Person', function(){
-        it('no person in the NLP object should result in description: event, organization', function(){
-            let noPerson = [nlpObject[0], nlpObject[3], nlpObject[4], nlpObject[5]];
-            assert.equal(createCalendarEvent(noPerson, ocrRes).description, "Dentist appointment, Peter O Toole");
-        });
+  describe('No Person', function() {
+    it('no person in the NLP object should result in description: event, organization', function() {
+      let noPerson = [nlpObject[0], nlpObject[3], nlpObject[4], nlpObject[5]];
+      assert.equal(createCalendarEvent(noPerson, ocrRes).description, 'Dentist appointment, Peter O Toole');
     });
+  });
 
-    describe('No organization', function(){
-        it('no organization in the NLP object should result in description: event, person', function(){
-            let noOrganization = [nlpObject[0], nlpObject[1], nlpObject[2], nlpObject[3], nlpObject[4]];
-            assert.equal(createCalendarEvent(noOrganization, ocrRes).description, "Dentist appointment, T.E. Lawrence");
-        });
+  describe('No organization', function() {
+    it('no organization in the NLP object should result in description: event, person', function() {
+      let noOrganization = [nlpObject[0], nlpObject[1], nlpObject[2], nlpObject[3], nlpObject[4]];
+      assert.equal(createCalendarEvent(noOrganization, ocrRes).description, 'Dentist appointment, T.E. Lawrence');
     });
-
+  });
 });
